3e38fedd60c8a98be82ab5927142ed8cdcd911b9
swfree <skye.free@gmail.com>
2016-03-11 16:11:10 -0800

    Working transition

--- client/watchout.js
+++ client/watchout.js
@@ -1,48 +1,52 @@
-var boardWidth = 600;
-var boardHeight = 600;
-// HELPER FUNCTIONS
+var boardLength = 600;
+var numberOfEnemies = 10;
+
 var initial = function(x, y, r) {
   svg.append('circle').attr('cx', x).attr('cy', y).attr('r', r);
 };
 
-
-var randX = function() {
-  return Math.random() * boardWidth;
-};
-
-var randY = function() {
-  return Math.random() * boardHeight;
+var randPositions = function() {
+  var result = [];
+  for (var i = 0; i < numberOfEnemies; i++) {
+    result.push(Math.random() * boardLength);
+  }
+  return result;
 };
 
 var drag = d3.behavior.drag()
   .on('drag', function() { mouse.attr('cx', d3.event.x).attr('cy', d3.event.y); });
 
-
-
-// start slingin' some d3 here.
-
-var xPositions = [randX(), randX(), randX(), randX(), randX(), randX(), randX(), randX(), randX(), randX()];
-var yPositions = [randY(), randY(), randY(), randY(), randY(), randY(), randY(), randY(), randY(), randY()];
-
-
 var svg = d3.select('.board').append('svg')
-  .attr('width', boardWidth)
-  .attr('height', boardHeight);
+  .attr('width', boardLength)
+  .attr('height', boardLength);
 
 d3.select('svg').selectAll('circle')
-  .data(xPositions).enter().append('circle').attr('cx', function(d) { return d; })
+  .data(randPositions()).enter().append('circle').attr('cx', function(d) { return d; })
   .attr('r', 10)
-  .data(yPositions).attr('cy', function(d) { return d; });
+  .data(randPositions()).attr('cy', function(d) { return d; })
+  .classed('enemies', true);
 
 // var circle = svg.append('circle').attr('cx', 30).attr('cy', 30).attr('r', 10);
 // circle.transition().attr('cx', 60).duration(1000).delay(1000);
 
 var mouse = d3.select('svg').append('circle')
-  .attr('fill', 'blue').attr('cx', boardWidth / 2)
-  .attr('cy', boardHeight / 2).attr('r', 10)
-  .classed('mouse', true);
-
-d3.select('.mouse').call(drag);
+  .attr('fill', 'blue').attr('cx', boardLength / 2)
+  .attr('cy', boardLength / 2).attr('r', 10)
+  .classed('mouse', true).call(drag);
 
 
+var move = function () {
+  d3.select('svg').selectAll('.enemies')
+    .data(randPositions())
+    .transition().duration(1500)
+    .attr('cx', function(d) { return d; })
+    .each(function() {
+      d3.selectAll('.enemies')
+      .data(randPositions())
+      .transition()
+      .attr('cy', function(d) { return d; });
+    });
 
+};
+
+setInterval(move, 2000);
